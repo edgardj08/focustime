4cadc89a6c2483b9dc78338fcbd765e745d07278
Edgard J. Concha Medina <econcha@midasconsultores.com.ar>
2021-06-14 21:47:34 -0300

    inicial

---
+++ src/features/focus/FocusHistory.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, Text, SafeAreaView } from 'react-native';
+
+import { fontSizes, spacing } from '../../utils/sizes';
+
+import { RoundedButton } from '../../components/RoundedButton';
+
+const HistoryItem = ({ item, index }) => {
+  return <Text style={styles.historyItem(item.status)}>{item.subject}</Text>;
+};
+
+export const FocusHistory = ({ focusHistory, onClear }) => {
+  const clearHistory = () => {
+    onClear();
+  };
+
+  return (
+    <>
+      <SafeAreaView style={{ flex: 0.5, alignItems: 'center' }}>
+        <Text style={styles.title}>Things weve focused on</Text>
+
+        {!!focusHistory.length && (
+          <>
+            <FlatList
+              stylee={{ flex: 1 }}
+              contentContainerStyle={{ flex: 1, alignItems: 'center' }}
+              data={focusHistory}
+              renderItem={HistoryItem}
+            />
+
+            <View style={styles.clearContainer}>
+              <RoundedButton
+                size={75}
+                title="clear"
+                onPress={() => {
+                  onClear();
+                }}
+              />
+            </View>
+          </>
+        )}
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  historyItem: (status) => ({
+    color: status > 1 ? 'red' : 'green',
+    fontSizes: fontSizes.md,
+  }),
+  title: {
+    color: 'white',
+    fontSize: fontSizes.lg,
+  },
+  clearContainer: {
+    alignItems: 'center',
+    padding: spacing.md,
+  },
+});
